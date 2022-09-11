e51827be3e3ed9760b78f3f4cbfa7e5aecb8c47a
wonjin <wonjine7@naver.com>
2022-09-11 21:36:40 +0900

    날씨 정보 가져오기

---
+++ weather-app/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+// 앱이 시작되자마자  현재위치기반의 날씨가 보인다
+// 날씨정보에는 도시, 섭씨, 화씨 날씨상태
+// 5개의 버튼이 있다 (1개는 현재위치,4개는 다른도시)
+// 도시버튼을 클릭할때 마다 도시별 날씨상태가 나온다
+// 현재위치 버튼을 누르면 다시 현재위치 기반의 날씨정보가 나온다
+// 데이터를 들고오는 동안 로딩 스피너가 돈다
+
+function App() {
+const getCurrentLocation=()=>{
+  navigator.geolocation.getCurrentPosition((position)=>{
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    
+  });
+};
+
+const getWeatherByCurrentLocation = async(lat,lon) => {
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=8f9165131e11c92e924fec7921bcd238`;
+  let response = await fetch(url);
+  let data = await response.json();
+  console.log("data",data);
+}
+
+  useEffect(()=>{
+    getCurrentLocation()
+  },[]);
+
+  return (
+    <div>
+      h1h1
+    </div>
+  );
+}
+
+export default App;
